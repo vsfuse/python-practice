91259dd92acaf302e25f3b0dc15c066b8bc4d392
vsfuse <vishnu.gaur@sourcefuse.com>
2018-12-07 12:47:17 +0530

    average of a list

---
+++ average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Average:
+    def avg(self):
+        n = 0
+        for i in list1:
+            n += i
+
+        count = len(list1)
+        avg = n/count
+        print(avg)
+
+
+
+list1 = [4,5,6,7,8,9,12,11,14]
+
+p1 = Average()
+p1.avg()
